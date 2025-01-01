59c37580f5094c5b83ea622a1cf1c33ae18c4a1e
krzysbielsko <krzysbielsko@gmail.com>
2025-01-01 19:26:54 +0100

    Fixed issues regarding super triangle.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from file_input import save_division_to_file
-from graphical_input import draw_polygonal_division
+from graphical_input import set_polygonal_division
 
-division = draw_polygonal_division()
+division = set_polygonal_division()
 save_division_to_file(division, "test_output.txt")
