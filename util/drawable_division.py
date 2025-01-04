4f616f55fa4f6af4b5958e9010fa59a43aa4b544
KK04 <72666836+krzysztof-kopel@users.noreply.github.com>
2025-01-04 01:15:45 +0100

    Changed approach to the relationship between Division and DrawableDivision.

--- util/drawable_division.py
+++ util/drawable_division.py
@@ -1,10 +1,9 @@
 from util.planar_division import Division
 
 class DrawableDivision(Division):
-    def __init__(self):
+    def __init__(self, division: Division):
         super().__init__()
-        self.points_to_mark = []
-        self.triangle_to_color = None
+        self.copy_data_from_division(division)
 
     def copy_data_from_division(self, division: Division):
         self.polygons = division.polygons
