3d8a6a1f7a7e350582ff5487ad3ce17563161f87
KK04 <72666836+krzysztof-kopel@users.noreply.github.com>
2025-01-04 01:10:59 +0100

    A method to copy data from a normal division.

--- input_output/division_drawer.py
+++ input_output/division_drawer.py
@@ -1,12 +1,21 @@
-import matplotlib.pyplot as plt
+from copy import deepcopy
 
-from util.planar_division import Division
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Button
+
+from util.drawable_division import DrawableDivision
+
 
 class DivisionDrawer:
-    def __init__(self, division, with_supertriangle):
-        self.division = division
-        self.fig, self.ax = plt.subplots()
+    def __init__(self, divisions, with_supertriangle):
+        if type(divisions) is DrawableDivision:
+            self.divisions = [divisions]
+        else:
+            self.divisions = divisions
+        self.current_division_num = 0
+        self.fig, self.ax = plt.subplots(figsize=(10, 10))
         self.ax.set_aspect('equal')
+        self.ax.set_title("Użyj przycisków na dole ekranu, aby oglądać kolejne kroki wizualizacji")
         self.supertriangle = with_supertriangle
         if not with_supertriangle:
             self.ax.set_xlim(min(point[0] for polygon in division.polygons[1:] for point in polygon.points) - 5,
@@ -19,13 +28,40 @@ class DivisionDrawer:
             self.ax.set_ylim(min(point[1] for polygon in division.polygons for point in polygon.points) - 5,
                              max(point[1] for polygon in division.polygons for point in polygon.points) + 5)
 
-    def draw(self, with_triangles):
-        self.ax.plot(self.division.searched_point[0], self.division.searched_point[1], 'go')
+        # Buttons for navigation
+        self.ax_next = plt.axes((0.6, 0.02, 0.05, 0.05))  # Next button
+        self.ax_prev = plt.axes((0.4, 0.02, 0.05, 0.05))  # Previous button
+
+        self.button_next = Button(self.ax_next, '→')
+        self.button_prev = Button(self.ax_prev, '←')
+
+        self.button_next.on_clicked(self.next_division)
+        self.button_prev.on_clicked(self.prev_division)
+
+    def start_config(self):
+        self.ax.set_aspect('equal')
+        self.ax.set_title("Użyj przycisków na dole ekranu, aby oglądać kolejne kroki wizualizacji")
+
+    def next_division(self, event):
+        if self.current_division_num < len(self.divisions) - 1:
+            self.current_division_num += 1
+            self.draw(self.supertriangle, self.current_division_num)
+
+    def prev_division(self, event):
+        if self.current_division_num > 0:
+            self.current_division_num -= 1
+            self.draw(self.supertriangle, self.current_division_num)
+
+    def draw(self, with_triangles=True, div_number=0):
+        current_division = self.divisions[div_number]
+        self.ax.clear()
+        self.start_config()
+        self.ax.plot(current_division.searched_point[0], current_division.searched_point[1], 'go')
         black_lines = set()
         if self.supertriangle:
-            polygons = self.division.polygons
+            polygons = current_division.polygons
         else:
-            polygons = self.division.polygons[1:]
+            polygons = current_division.polygons[1:]
         for polygon in polygons:
             for i in range(len(polygon.points)):
                 point = polygon.points[i]
@@ -36,31 +72,34 @@ class DivisionDrawer:
                 black_lines.add((polygon.points[(i + 1) % len(polygon.points)], point))
             if with_triangles:
                 for triangle in polygon.triangles:
-                    if (triangle.coordinates[0], triangle.coordinates[1]) not in black_lines and (triangle.coordinates[1], triangle.coordinates[0]) not in black_lines:
+                    if (triangle.coordinates[0], triangle.coordinates[1]) not in black_lines and (
+                    triangle.coordinates[1], triangle.coordinates[0]) not in black_lines:
                         x = (triangle.coordinates[0][0], triangle.coordinates[1][0])
                         y = (triangle.coordinates[0][1], triangle.coordinates[1][1])
                         self.ax.plot(x, y, 'r-')
 
-                    if (triangle.coordinates[1], triangle.coordinates[2]) not in black_lines and (triangle.coordinates[2], triangle.coordinates[1]) not in black_lines:
+                    if (triangle.coordinates[1], triangle.coordinates[2]) not in black_lines and (
+                    triangle.coordinates[2], triangle.coordinates[1]) not in black_lines:
                         x = (triangle.coordinates[1][0], triangle.coordinates[2][0])
                         y = (triangle.coordinates[1][1], triangle.coordinates[2][1])
                         self.ax.plot(x, y, 'r-')
 
-                    if (triangle.coordinates[2], triangle.coordinates[0]) not in black_lines and (triangle.coordinates[0], triangle.coordinates[2]) not in black_lines:
+                    if (triangle.coordinates[2], triangle.coordinates[0]) not in black_lines and (
+                    triangle.coordinates[0], triangle.coordinates[2]) not in black_lines:
                         x = (triangle.coordinates[2][0], triangle.coordinates[0][0])
                         y = (triangle.coordinates[2][1], triangle.coordinates[0][1])
                         self.ax.plot(x, y, 'r-')
         self.fig.canvas.draw()
-        return plt
+        plt.show()
 
 def draw_polygonal_division(division: Division, with_triangles: bool = False):
     drawer = DivisionDrawer(division, True)
-    drawer.draw(with_triangles=with_triangles).show()
+    drawer.draw(with_triangles, 0)
 
 if __name__ == "__main__":
     from file_input import get_division_from_file
     division = get_division_from_file("../input_output_files/test_output_presentation.txt")
+    division2 = deepcopy(division)
     division.triangulate_all()
-    drawer = DivisionDrawer(division, True)
-    plt = drawer.draw(with_triangles=True)
-    plt.show()
+    drawer = DivisionDrawer([division2, division], True)
+    drawer.draw()
