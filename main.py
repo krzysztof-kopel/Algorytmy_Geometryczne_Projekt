9731992015fd421a55e6c0965745311a25d41213
KK04 <72666836+krzysztof-kopel@users.noreply.github.com>
2025-01-04 02:09:22 +0100

    Improved main

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from input_output.graphical_input import set_polygonal_division
 
 division = set_polygonal_division()
 topTriangleNode, drawer = hierarchy(division, True, division.searched_point)
-drawer.draw()
 resultant_polygon = find_smallest_triangle_in_hierarchy(topTriangleNode, division.searched_point)
-print(resultant_polygon.id)
+print(f"Poszukiwany punkt znajduje się w wielokącie nr {resultant_polygon.id}")
+drawer.draw()
